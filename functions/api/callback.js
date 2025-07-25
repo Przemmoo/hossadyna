052c6b472072b5f4bab6abe33a94939640f11e1b
Przemo Kilan <przemmoo@gmail.com>
2025-07-25 14:21:23 +0200

    Switch to implicit OAuth flow for Decap CMS

--- functions/api/callback.js
+++ functions/api/callback.js
@@ -17,30 +17,36 @@ export async function onRequestGet(context) {
 
   try {
     // Exchange code for access token
-    const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        client_id: env.GITHUB_CLIENT_ID,
-        client_secret: env.GITHUB_CLIENT_SECRET,
-        code: code,
-      }),
-    });
+    const tokenResponse = await fetch(
+      'https://github.com/login/oauth/access_token',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          client_id: env.GITHUB_CLIENT_ID,
+          client_secret: env.GITHUB_CLIENT_SECRET,
+          code: code,
+        }),
+      }
+    );
 
     const tokenData = await tokenResponse.json();
 
     if (tokenData.error) {
       console.error('Token exchange error:', tokenData.error_description);
-      return Response.redirect(`${url.origin}/admin/#error=${tokenData.error}`, 302);
+      return Response.redirect(
+        `${url.origin}/admin/#error=${tokenData.error}`,
+        302
+      );
     }
 
     // Redirect back to admin with token as URL fragment (for security)
     const adminUrl = new URL('/admin/', url.origin);
     adminUrl.hash = `access_token=${tokenData.access_token}&token_type=bearer`;
-    
+
     return Response.redirect(adminUrl.toString(), 302);
   } catch (error) {
     console.error('Callback error:', error);
